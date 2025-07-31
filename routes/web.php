17c5d197b0d9f4b1b405d1d2a0ba552138c96a39
mando <143672248+Sumandosilalahi2929@users.noreply.github.com>
2025-07-31 14:36:38 +0700

    feat: crud cms

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
+// Redirect the root URL to the contacts index page
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('contacts.index');
 });
+
+
+Route::resource('contacts', ContactController::class);
